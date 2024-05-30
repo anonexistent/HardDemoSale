4a49f970a1fc12b1e606cd76647877b40c1fd31f
anonexistent <wwwzlx86@gmail.com>
2024-05-30 19:02:00 +0500

    sуnсfusiоff (on) adventures

--- DemoSale/Pages/PktEditPage.xaml.cs
+++ DemoSale/Pages/PktEditPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using DemoSale.Data;
 using DemoSale.DataBaseCore;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -42,38 +44,37 @@ namespace DemoSale.Pages
         private void InitCurrentItemForChange(Pkt cifc)
         {
 
-            tbKontr.Text = cifc.seller.ToString();
-            tbGr.Text = cifc.sellerAgent;
-            tbArea.Text = cifc.region;
-            tbManager.Text = cifc.manager;
-            cbPosTypes.SelectedItem = db.PositionType.Where(x => x.positionName == cifc.positionType).FirstOrDefault();
-            cbPosName.Text = cifc.positionName;
-            tbCount.Text = cifc.count.ToString();
+            tbKontr.Text                    = cifc.seller.ToString();
+            tbGr.Text                       = cifc.sellerAgent;
+            tbArea.Text                     = cifc.region;
+            tbManager.Text                  = cifc.manager;
+            cbPosTypes.SelectedItem         = db.PositionType.Where(x => x.positionName == cifc.positionType).FirstOrDefault();
+            cbPosName.Text                  = cifc.positionName;
+            tbCount.Text                    = cifc.count.ToString();
 
-            cbDealer.SelectedItem = db.Dealer.Where(x => x.dealerName == cifc.dealer).FirstOrDefault();
-            tbMoneyZakup.Value = (decimal?)cifc.purchaseMoney;
-            tbMoneyDealer.Value = (decimal?)cifc.paidMoney;
-            tbMoneyDealerDebt.Value = (decimal?)cifc.deptMoney;
-            dpPayTerm.SelectedDate = DateTime.Parse(cifc.paymentTerm.ToString());
-            cbSpec.SelectedItem = db.Specification.FirstOrDefault(x=>x.name==cifc.specification);
+            cbDealer.SelectedItem           = db.Dealer.Where(x => x.dealerName == cifc.dealer).FirstOrDefault();
+            tbMoneyZakup.Value              = (decimal?)cifc.purchaseMoney;
+            tbMoneyDealer.Value             = (decimal?)cifc.paidMoney;
+            tbMoneyDealerDebt.Value         = (decimal?)cifc.deptMoney;
+            dpPayTerm.SelectedDate          = DateTime.Parse(cifc.paymentTerm.ToString());
+            cbSpec.SelectedItem             = db.Specification.FirstOrDefault(x=>x.name==cifc.specification);
 
-            tbPriceSellerDep.Value = (decimal?)cifc.salesDepartmentMoney;
-            tbPriceRealization.Value = (decimal?)cifc.realization;
-            tbPriceGotten.Value = (decimal?)cifc.arrivedMoney;
-            tbPriceGotten.Value = (decimal?)cifc.arrivedMoney;
-            tbPriceDebt.Value = (decimal?)cifc.realizationDept;
-            dpDateGiveMoney.SelectedDate = DateTime.Parse(cifc.paymentTermRealization.ToString());
-            cbPaymentMethod.Text = cifc.paymentMethod;
-            tbMarzh.Value = (decimal?)cifc.marginalProfit;
-            tbTrans.Value = (decimal?)cifc.transportOther;
-            tbTransNds.Value = (decimal?)cifc.transportOtherNds;
-            tbLoading.Value = (decimal?)cifc.loadingUnloading;
+            tbPriceSellerDep.Value          = (decimal?)cifc.salesDepartmentMoney;
+            tbPriceRealization.Value        = (decimal?)cifc.realization;
+            tbPriceGotten.Value             = (decimal?)cifc.arrivedMoney;
+            tbPriceDebt.Value               = (decimal?)cifc.realizationDept;
+            dpDateGiveMoney.SelectedDate    = DateTime.Parse(cifc.paymentTermRealization.ToString());
+            cbPaymentMethod.Text            = cifc.paymentMethod;
+            tbMarzh.Value                   = (decimal?)cifc.marginalProfit;
+            tbTrans.Value                   = (decimal?)cifc.transportOther;
+            tbTransNds.Value                = (decimal?)cifc.transportOtherNds;
+            tbLoading.Value                 = (decimal?)cifc.loadingUnloading;
 
-            tbKv.Value = (decimal?)cifc.kvMoney;
-            tbMoneyOther.Value = (decimal?)cifc.otherMoney;
-            tbOtherWork.Text = cifc.dopPositionDescription;
-            dpDeliveryTerm.SelectedDate = DateTime.Parse(cifc.deliveryDate.ToString());
-            tbForeCalc.Value = (decimal?)cifc.forCalculation;
+            tbKv.Value                      = (decimal?)cifc.kvMoney;
+            tbMoneyOther.Value              = (decimal?)cifc.otherMoney;
+            tbOtherWork.Text                = cifc.dopPositionDescription;
+            dpDeliveryTerm.SelectedDate     = DateTime.Parse(cifc.deliveryDate.ToString());
+            tbForeCalc.Value                = (decimal?)cifc.forCalculation;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -85,14 +86,10 @@ namespace DemoSale.Pages
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            Type t = cifc.GetType();
-            var props = t.GetProperties();
-
-            foreach (var item in props)
-            {
-                MessageBox.Show(item.Name +"\t" + item.GetValue(cifc).ToString());
-            }
-
+            var newItem = GetNewPktFromForm();
+            //  отличиающиеся поля
+            var listDiff = GetDifferencesFieldName(cifc, newItem);
+            
             string message = "Принять следующие изменения?" +
                 "1. -" +
                 "2. -" +
@@ -103,6 +100,79 @@ namespace DemoSale.Pages
 
             }
             FrameClass.mainFrame.Navigate(new PktMainPage());
+        }
+
+        private List<string> GetDifferencesFieldName(Pkt original, Pkt newItem)
+        {
+            var result = new List<string>();
+
+            Type tOrig = original.GetType();
+            var propsOrig = tOrig.GetProperties();
+            var tNew = newItem.GetType();
+            var propsNew = tNew.GetProperties();
+
+            foreach (var prop in propsOrig)
+            {
+                var tempOrig = prop.GetValue(original);
+                var tempNew = prop.GetValue(newItem);
+                if(tempNew != tempOrig)
+                {
+                    MessageBox.Show($"i found a difference:\n\n([{prop}] {tempOrig} — {tempNew})","", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                }
+            }
+
+            //foreach (var item in props)
+            //{
+            //    MessageBox.Show(item.Name + "\t" + item.GetValue(original).ToString());
+            //}
+
+            return result;
+        }
+
+        private Pkt GetNewPktFromForm()
+        {
+            Pkt result = new()
+            {
+                seller = tbKontr.Text,
+                sellerAgent = tbGr.Text,
+                region = tbArea.Text,
+                manager = tbManager.Text,
+                positionType = (cbPosTypes.SelectedItem as PositionType).positionName,
+                positionName = cbPosName.Text,
+                count = int.Parse(tbCount.Text),
+
+                dealer = (cbDealer.SelectedItem as Dealer).dealerName,
+                purchaseMoney = (double)tbMoneyZakup.Value,
+                paidMoney = (double)tbMoneyDealer.Value,
+                deptMoney = (double)tbMoneyDealerDebt.Value,
+                paymentTerm = DateOnly.Parse(dpPayTerm.SelectedDate.Value.Date.ToString()),
+                specification = (cbSpec.SelectedItem as Specification).name,
+
+                salesDepartmentMoney = (double)tbPriceSellerDep.Value,
+                realization = (double)tbPriceRealization.Value,
+                arrivedMoney = (double)tbPriceGotten.Value,
+                //result.realizationDept    =tbPriceDebt.Value           ;
+                paymentTermRealization = DateOnly.Parse(dpDateGiveMoney.SelectedDate.Value.Date.ToString()),
+                paymentMethod = cbPaymentMethod.Text,
+                marginalProfit = (double)tbMarzh.Value,
+                transportOther = (double)tbTrans.Value,
+                transportOtherNds = (double)tbTransNds.Value,
+                loadingUnloading = (double)tbLoading.Value,
+
+                kvMoney = (double)tbKv.Value,
+                otherMoney = (double)tbMoneyOther.Value,
+                dopPositionDescription = tbOtherWork.Text,
+                deliveryDate = DateOnly.Parse(dpDeliveryTerm.SelectedDate.Value.Date.ToString()),
+                forCalculation = (double)tbForeCalc.Value
+            };
+
+            return result;
+        }
+
+        private void tbCount_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
